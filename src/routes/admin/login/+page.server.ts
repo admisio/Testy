77f9b2105bb9636058d3857db7f1d9ffaa5502e2
Sebastian Pravda <sebastian.pravda@gmail.com>
2023-02-07 23:55:41 +0100

    fix: password auth

--- src/routes/admin/login/+page.server.ts
+++ src/routes/admin/login/+page.server.ts
@@ -6,25 +6,29 @@ import jwt from 'jsonwebtoken';
 import type { Actions } from './$types';
 
 export const actions: Actions = {
-  default: async ({ request, cookies }) => {
-    try {
-      const data = await request.formData();
-      const username = data.get('username') as string;
-      const password = data.get('password') as string;
+    default: async ({ request, cookies }) => {
+        try {
+            const data = await request.formData();
+            const username = data.get('username') as string;
+            const password = data.get('password') as string;
 
-      const { id, password: passwordHash } = await prisma.admin.findFirstOrThrow({
-        where: { username },
-        select: { id: true, password: true }
-      });
-      await bcrypt.compare(password, passwordHash);
+            const { id, password: passwordHash } = await prisma.admin.findFirstOrThrow({
+                where: { username },
+                select: { id: true, password: true }
+            });
+            if (!(await bcrypt.compare(password, passwordHash))) {
+                return fail(401, { message: 'Authentication failed', incorrect: true });
+            }
 
-      cookies.set('jwt', jwt.sign({ id: id, name: username, role: 'admin' }, JWT_SECRET), { path: '/' });
+            cookies.set('jwt', jwt.sign({ id: id, name: username, role: 'admin' }, JWT_SECRET), {
+                path: '/'
+            });
 
-      return { success: true };
-      // 👆 or, if we're using HTTP headers based auth, we could return the token,
-      // and let the client set the header on subsequent requests
-    } catch {
-      return fail(401, { message: 'Authentication failed', incorrect: true });
+            return { success: true };
+            // 👆 or, if we're using HTTP headers based auth, we could return the token,
+            // and let the client set the header on subsequent requests
+        } catch {
+            return fail(401, { message: 'Authentication failed', incorrect: true });
+        }
     }
-  }
 };
