e2d23f601ff84649dd6ed69c6a16804c415c3266
EETagent <vojta.jungmann@gmail.com>
2023-02-12 15:42:00 +0100

    feat: redirect

--- src/routes/(user)/(authenticated)/home/+layout.server.ts
+++ src/routes/(user)/(authenticated)/home/+layout.server.ts
@@ -1,4 +1,6 @@
 import type { LayoutServerLoad } from './$types';
+import { redirect } from '@sveltejs/kit';
+
 import { router } from '$lib/trpc/router';
 import { createContext } from '$lib/trpc/context';
 
@@ -7,6 +9,6 @@ export const load: LayoutServerLoad = async (event) => {
         const auth = await router.createCaller(await createContext(event)).auth.user();
         return { isAuthenticated: true, userId: auth };
     } catch {
-        return { isAuthenticated: false };
+        throw redirect(302, '/auth/login');
     }
 };
