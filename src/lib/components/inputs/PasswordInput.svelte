c3f80da38a97720f2685106822d185665a81826f
EETagent <vojta.jungmann@gmail.com>
2023-02-13 18:00:32 +0100

    feat: add multiple styled inputs

---
+++ src/lib/components/inputs/PasswordInput.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import TextInput from './TextInput.svelte';
+
+    export let required: boolean = false;
+</script>
+
+<TextInput
+    icon="ic:outline-remove-red-eye"
+    type="password"
+    placeholder="Heslo"
+    name="password"
+    {required}
+/>
