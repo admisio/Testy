3bc8778f81f0693023dcbdafcd45bcade215308c
EETagent <vojta.jungmann@gmail.com>
2023-02-13 18:00:40 +0100

    feat: add submit button

---
+++ src/lib/components/buttons/Submit.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import Icon from '@iconify/svelte';
+</script>
+
+<button type="submit">
+    <div>
+        <span class="mr-1 font-bold text-white">Přihlásit se</span>
+        <Icon icon="material-symbols:arrow-forward-rounded" />
+    </div>
+</button>
+
+<style lang="postcss">
+    button {
+        @apply ml-3 inline-block rounded-lg bg-[#3580b7];
+        @apply px-5 py-3;
+        @apply text-sm font-bold text-white no-underline;
+        @apply shadow-xl shadow-sky-500/20 transition-all duration-300;
+    }
+    button:hover {
+        @apply bg-[#3073a6];
+    }
+    div {
+        @apply flex flex-row items-center;
+    }
+</style>
