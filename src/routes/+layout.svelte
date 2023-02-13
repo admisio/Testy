e1365cc81836e46b239634829168e5cd7b3e58b7
EETagent <vojta.jungmann@gmail.com>
2023-02-13 18:01:00 +0100

    fix: global layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,15 +10,13 @@
 />
 
 <template>
-    <main class="container">
+    <main>
         <slot />
     </main>
 </template>
 
 <style windi:preflights:global windi:safelist:global>
-    :global(html) {
-        /* font-family: 'Poppins', sans-serif; */
-        background-color: #111;
-        color: #e3e3e3;
-    }
+    /* :global(html) {
+         font-family: 'Poppins', sans-serif; 
+    } */
 </style>
