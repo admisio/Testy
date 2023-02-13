baa274358ec7f5808ae943d42b5530074815731c
EETagent <vojta.jungmann@gmail.com>
2023-02-13 18:08:59 +0100

    feat: add username input

---
+++ src/lib/components/inputs/UsernameInput.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import TextInput from './TextInput.svelte';
+
+    export let required: boolean = false;
+    export let placeholder: string = 'Uživatel';
+</script>
+
+<TextInput
+    icon="material-symbols:person-2-outline-rounded"
+    type="username"
+    {placeholder}
+    name="username"
+    {required}
+/>
