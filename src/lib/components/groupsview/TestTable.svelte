18457088f84dfdd6c10d9f5d8d99f9d9de649c64
EETagent <vojta.jungmann@gmail.com>
2023-02-15 01:33:58 +0100

    feat: style test table

--- src/lib/components/groupsview/TestTable.svelte
+++ src/lib/components/groupsview/TestTable.svelte
@@ -1,22 +1,16 @@
 <script lang="ts">
-    import { formatDate, formatTime, remainingTime } from '$lib/utils/date';
-    import type { TestSubmission } from '@prisma/client';
+    import { formatDate, remainingTime } from '$lib/utils/date';
+    import type { Prisma } from '@prisma/client';
     import { createEventDispatcher } from 'svelte';
 
-    export let assignedTests: Array<{
-        id: number;
-        test: { title: string };
-        started: boolean;
-        startTime: Date | null;
-        endTime: Date | null;
-        submissions: (TestSubmission & {
-            user: {
-                id: number;
-                name: string;
-                surname: string;
+    export let assignedTests: Array<
+        Prisma.AssignedTestGetPayload<{
+            include: {
+                test: true;
+                submissions: true;
             };
-        })[];
-    }>;
+        }>
+    >;
 
     export let userCount: number;
 
@@ -45,59 +39,98 @@
     };
 </script>
 
-<table>
-    <thead>
-        <tr>
-            <th>Název</th>
-            <th>Čas spuštění</th>
-            <th>Čas ukončení</th>
-            <th>Zbývá (min)</th>
-            <th>Počet odevzdání</th>
-            <th>Spuštěn</th>
-        </tr>
-    </thead>
-    <tbody>
-        {#each assignedTests as assignedTest, i}
-            <tr>
-                <td>{assignedTest.test.title}</td>
-                <td>{formatDate(assignedTest.startTime)}</td>
-                <td>{formatDate(assignedTest.endTime)}</td>
-                <td>{timeRemaining[i]}</td>
-                <td>{`${assignedTest.submissions.length}/${userCount}`}</td>
-                <td
-                    >{assignedTest.started ? '✅ Ano' : '❌ Ne'}
-                    {#if !assignedTest.started}
-                        <button
-                            on:click={(_) => startTest(assignedTest.id)}
-                            class="text-center w-full">Spustit</button
-                        >
-                    {/if}
-                </td>
-            </tr>
-        {/each}
-    </tbody>
-</table>
+<div class="-mx-4 overflow-x-auto px-4 py-4 sm:-mx-8 sm:px-8">
+    <div class="inline-block min-w-full overflow-hidden rounded-lg shadow">
+        <table class="min-w-full leading-normal">
+            <thead>
+                <tr>
+                    <th
+                        class="border-b-2 border-gray-200 bg-gray-100 px-5 py-3 text-left text-xs font-semibold uppercase tracking-wider text-gray-600"
+                    >
+                        Název
+                    </th>
+                    <th
+                        class="border-b-2 border-gray-200 bg-gray-100 px-5 py-3 text-left text-xs font-semibold uppercase tracking-wider text-gray-600"
+                    >
+                        Čas spuštění
+                    </th>
+                    <th
+                        class="border-b-2 border-gray-200 bg-gray-100 px-5 py-3 text-left text-xs font-semibold uppercase tracking-wider text-gray-600"
+                    >
+                        Čas ukončení
+                    </th>
+                    <th
+                        class="border-b-2 border-gray-200 bg-gray-100 px-5 py-3 text-left text-xs font-semibold uppercase tracking-wider text-gray-600"
+                    >
+                        Zbývá (min)
+                    </th>
+                    <th
+                        class="border-b-2 border-gray-200 bg-gray-100 px-5 py-3 text-left text-xs font-semibold uppercase tracking-wider text-gray-600"
+                    >
+                        Počet odevzdání
+                    </th>
+                    <th
+                        class="border-b-2 border-gray-200 bg-gray-100 px-5 py-3 text-left text-xs font-semibold uppercase tracking-wider text-gray-600"
+                    >
+                        Spuštěn
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                {#each assignedTests as assignedTest, i}
+                    <tr>
+                        <td class="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+                            <div class="flex items-center">
+                                <p class="whitespace-no-wrap text-gray-900">
+                                    {assignedTest.test.title}
+                                </p>
+                            </div>
+                        </td>
+                        <td class="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+                            <p class="whitespace-no-wrap text-gray-900">
+                                {formatDate(assignedTest.startTime)}
+                            </p>
+                        </td>
+                        <td class="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+                            <p class="whitespace-no-wrap text-gray-900">
+                                {formatDate(assignedTest.endTime)}
+                            </p>
+                        </td>
+                        <td class="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+                            <p class="whitespace-no-wrap text-gray-900">
+                                {timeRemaining[i]}
+                            </p>
+                        </td>
+                        <td class="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+                            <p class="whitespace-no-wrap text-gray-900">
+                                {`${assignedTest.submissions.length}/${userCount}`}
+                            </p>
+                        </td>
+
+                        <td class="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+                            <span
+                                class="relative inline-block px-3 py-1 font-semibold leading-tight text-green-900"
+                            >
+                                {#if assignedTest.started}
+                                    <span
+                                        aria-hidden
+                                        class="absolute inset-0 rounded-full bg-green-200 opacity-50"
+                                    />
+                                    <span class="relative">Spuštěno</span>
+                                {:else}
+                                    <button
+                                        on:click={(_) => startTest(assignedTest.id)}
+                                        class="w-full text-center">Spustit</button
+                                    >
+                                {/if}
+                            </span>
+                        </td>
+                    </tr>
+                {/each}
+            </tbody>
+        </table>
+    </div>
+</div>
 
 <style lang="postcss">
-    table {
-        @apply border-2 border-[#1D1D1E] w-full;
-    }
-
-    th {
-        @apply bg-[#1D1D1E] text-white text-xl;
-        @apply px-4 py-4;
-        @apply border-2 border-[#1D1D1E];
-    }
-    td {
-        @apply p-2 text-xl;
-        @apply border-2 border-[#1D1D1E];
-        @apply bg-[#111111];
-    }
-    button {
-        @apply bg-green-700 text-white text-xl rounded-md;
-    }
-
-    tr:nth-child(even) {
-        background-color: #f2f2f2;
-    }
 </style>
