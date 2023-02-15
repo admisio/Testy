8fa0923c4631ee535e4269ff297e4ff76cf2efbd
EETagent <vojta.jungmann@gmail.com>
2023-02-15 17:19:24 +0100

    feat: add test submitting

--- src/routes/admin/(authenticated)/tests/+page.server.ts
+++ src/routes/admin/(authenticated)/tests/+page.server.ts
@@ -1,3 +1,8 @@
+import { fail } from '@sveltejs/kit';
+import type { Actions } from './$types';
+import { parseMd } from '$lib/utils/mdParser';
+import { createTest } from '$lib/trpc/services/testService';
+
 import { createContext } from '$lib/trpc/context';
 import { router } from '$lib/trpc/router';
 import type { PageServerLoad } from './$types';
@@ -7,3 +12,24 @@ export const load: PageServerLoad = async (event) => ({
         .createCaller(await createContext(event))
         .tests.list(event.url.searchParams.get('q') || undefined)
 });
+
+export const actions: Actions = {
+    default: async ({ request }) => {
+        try {
+            const data = await request.formData();
+            const title = data.get('title') as string;
+            const files = data.getAll('file').map((file) => file as Blob);
+
+            console.log(title);
+            const mdFile = files.find((file) => file.name.endsWith('.md')) as Blob;
+
+            console.log(mdFile);
+
+            const test = await parseMd(await mdFile.text());
+            const createResult = await createTest(test);
+            return createResult;
+        } catch {
+            return fail(500, { message: 'Parsing failed', incorrect: true });
+        }
+    }
+};
--- src/routes/admin/(authenticated)/tests/+page.svelte
+++ src/routes/admin/(authenticated)/tests/+page.svelte
@@ -1,14 +1,70 @@
 <script lang="ts">
-    import { goto } from "$app/navigation";
-    import type { PageData } from "./$types";
+    import { trpc } from '$lib/trpc/client';
+    import Button from '$lib/components/buttons/Button.svelte';
+    import TextInput from '$lib/components/inputs/TextInput.svelte';
+    import Modal from '$lib/components/Modal.svelte';
 
+    import { enhance } from '$app/forms';
+
+    import type { PageData } from './$types';
+    import Submit from '$lib/components/buttons/Submit.svelte';
+    import TestCard from '$lib/components/test/TestCard.svelte';
+    import { invalidateAll } from '$app/navigation';
 
     export let data: PageData;
 
+    const openModal = async () => {
+        isModalOpen = true;
+    };
+
+    const closeModal = async () => {
+        isModalOpen = false;
+    };
+
+    let isModalOpen = false;
+
+    const deleteTest = async (id: number) => {
+        await trpc().tests.delete.mutate(id);
+        invalidateAll();
+    };
 </script>
 
-<h1 class="font-bold text-6xl">Testy</h1>
-{#each data.tests as test, i}
-    <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <p on:click={(_) => goto('/admin/tests/' + i)} class="hover:cursor-pointer text-red-700">{test.title}</p>
-{/each}
+{#if isModalOpen}
+    <Modal on:close={closeModal}>
+        <div class="min-w-screen-lg p-14">
+            <form method="POST" use:enhance>
+                <h1 class="my-6 text-4xl font-bold text-[#3580b7]">Vytvořit test</h1>
+                <TextInput name="title" type="text" placeholder="Název testu" />
+                <div
+                    class="my-6 h-48 p-6"
+                    style={`background-image: url("data:image/svg+xml,%3csvg width='100%25' height='100%25' fill='none' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' rx='9' ry='9'  stroke-opacity='50%' stroke='%23406280' stroke-width='4' stroke-dasharray='10' stroke-dashoffset='${0}' stroke-linecap='square'/%3e%3c/svg%3e");`}
+                >
+                    <span>TODO: Drag & Drop</span>
+                    <input type="file" name="file" id="file" multiple />
+                </div>
+                <Submit title="Přidat otázku" />
+            </form>
+        </div>
+    </Modal>
+{/if}
+
+<div
+    class="<md:flex-col mx-auto mx-auto mb-6 flex max-w-screen-xl  items-center px-4 px-4 py-3 md:px-6 md:px-6"
+>
+    <h1 class="<md:mb-3 text-6xl font-bold text-[#3580b7]">Testy</h1>
+    <div class="ml-3 mt-2">
+        <Button
+            on:click={openModal}
+            icon="material-symbols:add-circle-outline-rounded"
+            title="Přidat test"
+        />
+    </div>
+</div>
+
+<div
+    class="<md:flex-col mx-auto mx-auto flex max-w-screen-xl flex-wrap justify-between px-4 py-3 md:px-6"
+>
+    {#each data.tests as test}
+        <TestCard {test} on:delete={() => deleteTest(test.id)} />
+    {/each}
+</div>
