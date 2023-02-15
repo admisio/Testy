b938e32f3a5def2a88961eeaa962e5c75c3ba81e
EETagent <vojta.jungmann@gmail.com>
2023-02-15 17:19:02 +0100

    feat: remove margin left from the button component

--- src/routes/admin/(authenticated)/groups/+page.svelte
+++ src/routes/admin/(authenticated)/groups/+page.svelte
@@ -37,7 +37,7 @@
     class="<md:flex-col mx-auto mx-auto mb-6 flex max-w-screen-xl  items-center px-4 px-4 py-3 md:px-6 md:px-6"
 >
     <h1 class="<md:mb-3 text-6xl font-bold text-[#3580b7]">Skupiny</h1>
-    <div class="mt-2">
+    <div class="ml-3 mt-2">
         <Button
             on:click={createGroup}
             icon="material-symbols:add-circle-outline-rounded"
--- src/routes/admin/(authenticated)/users/+page.svelte
+++ src/routes/admin/(authenticated)/users/+page.svelte
@@ -13,7 +13,7 @@
     class="<md:flex-col mx-auto mx-auto mb-6 flex max-w-screen-xl  items-center px-4  py-3 md:px-6 md:px-6"
 >
     <h1 class="<md:mb-3 text-6xl font-bold text-[#3580b7]">Uživatelé</h1>
-    <div class="mt-2">
+    <div class="ml-3 mt-2">
         <Button
             on:click={addUser}
             icon="material-symbols:add-circle-outline-rounded"
