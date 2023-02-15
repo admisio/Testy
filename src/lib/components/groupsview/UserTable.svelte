c0abb37b7d9650c072aa0c9726b8142512c51505
EETagent <vojta.jungmann@gmail.com>
2023-02-15 01:27:07 +0100

    feat: style user table

--- src/lib/components/groupsview/UserTable.svelte
+++ src/lib/components/groupsview/UserTable.svelte
@@ -21,54 +21,79 @@
     >;
 </script>
 
-<table>
-    <thead>
-        <tr>
-            <th>Username</th>
-            {#each assignedTests as assignedTest}
-                <th>{assignedTest.test.title}</th>
-            {/each}
-            <th>Vše odevzdáno</th>
-        </tr>
-    </thead>
-    <tbody>
-        {#each users as user}
-            <tr>
-                <td class="font-bold">{user.username}</td>
-                {#each assignedTests as assignedTest}
-                    <td
-                        >{user.testSubmissions.find((s) => s.assignedTest.id === assignedTest.id)
-                            ? '✅ Odevzdáno'
-                            : '❌ Chybí'}</td
+<div class="-mx-4 overflow-x-auto px-4 py-4 sm:-mx-8 sm:px-8">
+    <div class="inline-block min-w-full overflow-hidden rounded-lg shadow">
+        <table class="min-w-full leading-normal">
+            <thead>
+                <tr>
+                    <th
+                        class="border-b-2 border-gray-200 bg-gray-100 px-5 py-3 text-left text-xs font-semibold uppercase tracking-wider text-gray-600"
                     >
+                        Uživatel
+                    </th>
+                    <th
+                        class="border-b-2 border-gray-200 bg-gray-100 px-5 py-3 text-left text-xs font-semibold uppercase tracking-wider text-gray-600"
+                    >
+                        Jméno
+                    </th>
+                    {#each assignedTests as assignedTest}
+                        <th
+                            class="border-b-2 border-gray-200 bg-gray-100 px-5 py-3 text-left text-xs font-semibold uppercase tracking-wider text-gray-600"
+                            >{assignedTest.test.title}</th
+                        >
+                    {/each}
+                    <th
+                        class="border-b-2 border-gray-200 bg-gray-100 px-5 py-3 text-left text-xs font-semibold uppercase tracking-wider text-gray-600"
+                    >
+                        Status
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                {#each users as user}
+                    <tr>
+                        <td class="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+                            <div class="flex items-center">
+                                <div class="ml-3">
+                                    <p class="whitespace-no-wrap text-gray-900">{user.username}</p>
+                                </div>
+                            </div>
+                        </td>
+                        <td class="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+                            <p class="whitespace-no-wrap text-gray-900">
+                                {user.name ?? 'Anonymní'}
+                            </p>
+                        </td>
+                        {#each assignedTests as assignedTest}
+                            <td class="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+                                >{user.testSubmissions.find(
+                                    (s) => s.assignedTest.id === assignedTest.id
+                                )
+                                    ? '✅ Odevzdáno'
+                                    : '❌ Chybí'}</td
+                            >
+                        {/each}
+                        <td class="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+                            <span
+                                class="relative inline-block px-3 py-1 font-semibold leading-tight text-green-900"
+                            >
+                                {#if user.testSubmissions.length === assignedTests.length}
+                                    <span
+                                        aria-hidden
+                                        class="absolute inset-0 rounded-full bg-green-200 opacity-50"
+                                    />
+                                    <span class="relative">Vše odevdzáno</span>
+                                {:else}
+                                    ❌
+                                {/if}
+                            </span>
+                        </td>
+                    </tr>
                 {/each}
-                <td>
-                    {user.testSubmissions.length === assignedTests.length ? '✅ ANO' : '❌ NE'}
-                    <!-- {#each user.testSubmissions as testSubmission}
-                    <td>{testSubmission}</td>
-                {/each} -->
-                </td></tr
-            >
-        {/each}
-    </tbody>
-</table>
+            </tbody>
+        </table>
+    </div>
+</div>
 
 <style lang="postcss">
-    table {
-        @apply w-full border-2 border-[#1D1D1E];
-    }
-
-    th {
-        @apply bg-[#1D1D1E] text-xl text-white;
-        @apply px-4 py-4;
-        @apply border-2 border-[#1D1D1E];
-    }
-    td {
-        @apply p-2 text-xl;
-        @apply border-2 border-[#1D1D1E];
-    }
-
-    tr:nth-child(even) {
-        background-color: #f2f2f2;
-    }
 </style>
