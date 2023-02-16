246cc097e8a161a57f6f2b9ecb9d98fe7846d546
Sebastian Pravda <sebastian.pravda@gmail.com>
2023-02-16 14:54:07 +0100

    feat: surnames in UserTable again

--- src/lib/components/groupsview/UserTable.svelte
+++ src/lib/components/groupsview/UserTable.svelte
@@ -65,7 +65,7 @@
                         </td>
                         <td class="border-b border-gray-200 bg-white px-5 py-5 text-sm">
                             <p class="whitespace-no-wrap text-gray-900">
-                                {user.name ?? 'Anonymní'}
+                                {user.name + ' ' + user.surname ?? 'Anonymní'}
                             </p>
                         </td>
                         {#each assignedTests as assignedTest}
