a4f370fc0c5d73d0a82d9ef4350ebdae605fadf4
EETagent <vojta.jungmann@gmail.com>
2023-02-16 14:48:47 +0100

    fix: add todo

--- src/routes/(user)/(authenticated)/home/+page.svelte
+++ src/routes/(user)/(authenticated)/home/+page.svelte
@@ -30,6 +30,7 @@
     <h1 class="<md:mb-3 text-6xl font-bold text-[#3580b7]">Přidělené aktivní testy</h1>
 </div>
 <div class="<md:flex-col mx-auto mx-auto mb-6 flex max-w-screen-xl px-4  py-3 md:px-6 md:px-6">
+    <!-- TODO: Věc backendu??? => Přesunout na backend validaci zda je test aktivní -->
     {#each assignedTests as assignedTest}
         {@const finished =
             (assignedTest.started &&
