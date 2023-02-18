2b80201a0b5dc1b1c2d98dd090f95f539b3b80ac
EETagent <vojta.jungmann@gmail.com>
2023-02-18 13:42:47 +0100

    feat: initial user adding

--- src/routes/admin/(authenticated)/users/+page.server.ts
+++ src/routes/admin/(authenticated)/users/+page.server.ts
@@ -1,7 +1,37 @@
 import { createContext } from '$lib/trpc/context';
 import { router } from '$lib/trpc/router';
+import { fail, type Actions } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async (event) => ({
     users: router.createCaller(await createContext(event)).users.list()
 });
+
+export const actions: Actions = {
+    default: async (event) => {
+        try {
+            const data = await event.request.formData();
+
+            const name = data.get('name')?.toString();
+            const surname = data.get('surname')?.toString();
+            const email = data.get('email')?.toString();
+            // TODO: Fix !
+            const username = data.get('username')!.toString();
+            const password = data.get('password')!.toString();
+
+            const createResult = await router
+                .createCaller(await createContext(event))
+                .users.create({
+                    name,
+                    surname,
+                    email,
+                    username,
+                    password
+                });
+            return createResult;
+        } catch (e) {
+            console.error(e);
+            return fail(500, { message: 'User creation failed', incorrect: true });
+        }
+    }
+};
