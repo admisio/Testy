407841e7958a1c0db2eaca04c574f120ee7ee9f5
EETagent <vojta.jungmann@gmail.com>
2023-02-18 03:35:36 +0100

    feat: better csv

--- src/lib/utils/csvExport.ts
+++ src/lib/utils/csvExport.ts
@@ -1,5 +1,5 @@
 import prisma from '$lib/prisma';
-import { createArrayCsvStringifier } from 'csv-writer';
+import { stringify as csvStringify } from 'csv-stringify/sync';
 
 export const exportCsv = async (): Promise<string> => {
     const users = await prisma.user.findMany({
@@ -44,52 +44,49 @@ export const exportCsv = async (): Promise<string> => {
             }
         }
     });
-    console.log(users);
-    const csvWriter = createArrayCsvStringifier({
-        header: [
-            'Database ID',
-            'Ev.č.',
-            'Jméno',
-            'Příjmení',
-            'Email',
-            'Název skupiny',
-            'Učitelé - JSON',
-            'Testy - JSON',
-            'První test',
-            'První test - body',
-            'Druhý test',
-            'Druhý test - body'
-        ]
-    });
-    const csv =
-        csvWriter.getHeaderString() +
-        csvWriter.stringifyRecords(
-            users.map((user) => {
-                const testsJson = user.testSubmissions.map((test) => {
-                    return {
-                        testTitle: test.assignedTest.test.title,
-                        startTime: test.assignedTest.startTime,
-                        endTime: test.assignedTest.endTime,
-                        evaluation: test.evaluation
-                    };
-                });
-                return [
-                    user.id,
-                    user.username,
-                    user.name,
-                    user.surname,
-                    user.email,
-                    user.group!.name,
-                    JSON.stringify(user.group!.admins.map((admin) => admin.admin)),
-                    JSON.stringify(testsJson),
-                    testsJson[0]?.testTitle,
-                    testsJson[0]?.evaluation,
-                    testsJson[1]?.testTitle,
-                    testsJson[1]?.evaluation
-                ];
-            })
-        );
-
+    const csv = csvStringify(
+        users.map((user) => {
+            const testsJson = user.testSubmissions.map((test) => {
+                return {
+                    testTitle: test.assignedTest.test.title,
+                    startTime: test.assignedTest.startTime,
+                    endTime: test.assignedTest.endTime,
+                    evaluation: test.evaluation
+                };
+            });
+            return [
+                user.id,
+                user.username,
+                user.name,
+                user.surname,
+                user.email,
+                user.group?.name,
+                JSON.stringify(user.group?.admins.map((admin) => admin.admin)),
+                JSON.stringify(testsJson),
+                testsJson[0]?.testTitle,
+                testsJson[0]?.evaluation,
+                testsJson[1]?.testTitle,
+                testsJson[1]?.evaluation
+            ];
+        }),
+        {
+            header: true,
+            columns: [
+                'Database ID',
+                'Ev.č.',
+                'Jméno',
+                'Příjmení',
+                'Email',
+                'Název skupiny',
+                'Učitelé - JSON',
+                'Testy - JSON',
+                'První test',
+                'První test - body',
+                'Druhý test',
+                'Druhý test - body'
+            ]
+        }
+    );
     console.log(csv);
     return csv;
 };
