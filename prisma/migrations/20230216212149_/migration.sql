50e56cc34db751d163b66bc7bd6ae8112d1f73c7
Sebastian Pravda <sebastian.pravda@gmail.com>
2023-02-21 18:33:51 +0100

    chore: clean prisma migrations

--- prisma/migrations/20230216212149_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `timeLimit` to the `Template` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Template" ADD COLUMN     "timeLimit" INTEGER NOT NULL;
--- prisma/migrations/20230220154227_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[userId,questionId,assignmentId]` on the table `Answer` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Answer_userId_questionId_assignmentId_key" ON "Answer"("userId", "questionId", "assignmentId");
