456ba8e81a4c809eff6e683519e16544c474efb0
EETagent <vojta.jungmann@gmail.com>
2023-02-23 21:24:23 +0100

    fix: builded project prisma import

--- apps/web/src/lib/prisma.ts
+++ apps/web/src/lib/prisma.ts
@@ -1,4 +1,13 @@
-import { PrismaClient } from '@prisma/client';
+import type { PrismaClient as ImportedPrismaClient } from '@prisma/client';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url ?? __filename);
+
+const { PrismaClient: RequiredPrismaClient } = require('@prisma/client');
+
+const _PrismaClient: typeof ImportedPrismaClient = RequiredPrismaClient;
+
+export class PrismaClient extends _PrismaClient {}
 
 const prisma = new PrismaClient();
 
