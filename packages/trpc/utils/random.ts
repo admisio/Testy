9c8c927de7c803bd6ae498f7627d5329857d1c40
Sebastian Pravda <sebastian.pravda@gmail.com>
2023-04-06 18:50:02 +0200

    feat: use Durstenfeld shuffle algorithm for random question order

--- packages/trpc/utils/random.ts
+++ packages/trpc/utils/random.ts
@@ -3,11 +3,11 @@ export const randomOrder = (length: number): number[] => {
     for (let i = 0; i < length; i++) {
         arr[i] = i;
     }
-    for (let i = 0; i < length; i++) {
-        const j = Math.floor(Math.random() * length);
-        const temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+    
+    // Durstenfeld shuffle algorithm (Fisher-Yates)
+    for (let i = length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
     }
     return arr;
 };
