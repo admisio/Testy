a057fabcf67e6fe12c71fd54480321f3913e6a82
EETagent <vojta.jungmann@gmail.com>
2023-04-07 22:43:45 +0200

    fix: use feedback instead of user

--- apps/web/src/routes/admin/(authenticated)/feedback/+page.svelte
+++ apps/web/src/routes/admin/(authenticated)/feedback/+page.svelte
@@ -11,13 +11,13 @@
     <h1 class="<md:mb-3 text-6xl font-bold text-[#3580b7]">Zpětná vazba</h1>
 </div>
 <div class="mx-auto mx-auto mb-6 flex max-w-screen-xl flex-col px-4 py-3 md:px-6 md:px-6">
-    {#each data.feedback as user}
+    {#each data.feedback as feedback}
         <div class="mb-2 w-full rounded-md bg-gray-300 p-2">
             <span class="rounded-md bg-white p-1">
-                {formatDate(user.createdAt)}
+                {formatDate(feedback.createdAt)}
             </span>
 
-            {user.content}
+            {feedback.content}
         </div>
     {/each}
 </div>
