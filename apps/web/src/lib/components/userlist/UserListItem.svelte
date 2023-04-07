d9c46b544f23ad58fba19bede27bbe920a84f4ec
EETagent <vojta.jungmann@gmail.com>
2023-04-07 22:25:05 +0200

    feat: improve user list, add settings wheel

---
+++ apps/web/src/lib/components/userlist/UserListItem.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import type { Prisma } from '@testy/database';
+    import UserSettings from './UserSettings.svelte';
+
+    export let user: Prisma.UserGetPayload<{
+        select: {
+            id: true;
+            username: true;
+            name: true;
+            email: true;
+        };
+    }>;
+</script>
+
+<div
+    class="flex items-center relative mx-3 mb-6 flex-grow basis-[32%] rounded-lg border border-gray-200 bg-white p-6 shadow"
+>
+    [ID {user.id}]
+
+    {user.username}
+
+    {user.name}
+
+    {user.email}
+
+    <div class="ml-auto">
+        <UserSettings />
+    </div>
+</div>
---
+++ apps/web/src/lib/components/userlist/UserSettings.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    import Icon from '@iconify/svelte';
+
+    import { clickoutside } from '@svelte-put/clickoutside';
+
+    let isOpen = false;
+
+    const open = () => {
+        isOpen = true;
+    };
+
+    const close = () => {
+        isOpen = false;
+    };
+</script>
+
+<div use:clickoutside on:clickoutside={close} class="relative flex flex-col">
+    <button on:click={open}><Icon icon="material-symbols:settings" /></button>
+    {#if isOpen}
+        <div class="menu absolute top-5 left-0 z-10 w-28">
+            <button class="!rounded-b-none">Nové heslo</button>
+            <button on:click={close} class="!rounded-t-none">Zavřít</button>
+        </div>
+    {/if}
+</div>
+
+<style lang="postcss">
+    .menu button {
+        @apply w-full;
+        @apply p-1 px-2;
+        @apply rounded-lg border border-gray-200 bg-white shadow;
+        @apply transition-colors duration-300;
+    }
+    .menu button:hover {
+        @apply bg-gray-100;
+    }
+</style>
