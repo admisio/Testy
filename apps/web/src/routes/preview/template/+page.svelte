9e1b89d9af412d40a9920261f46b628e84bb8473
EETagent <vojta.jungmann@gmail.com>
2023-04-13 22:21:42 +0200

    feat: code highlight for preview template route

--- apps/web/src/routes/preview/template/+page.svelte
+++ apps/web/src/routes/preview/template/+page.svelte
@@ -1,10 +1,21 @@
 <script lang="ts">
+    import { onMount } from 'svelte';
     import { enhance } from '$app/forms';
-    import TestPreview from '$lib/components/testpreview/TestPreview.svelte';
+
+    import hljs from 'highlight.js';
+    import 'highlight.js/styles/github.css';
 
     import type { ActionData } from './$types';
 
+    import TestPreview from '$lib/components/testpreview/TestPreview.svelte';
+
     export let form: ActionData;
+
+    onMount(() => {
+        document.querySelectorAll('.description code').forEach((el) => {
+            hljs.highlightElement(el as HTMLElement);
+        });
+    });
 </script>
 
 <form method="POST" use:enhance>
