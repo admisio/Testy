e5b720f4869d1b7bd53e4c5d68ee574a269ada0a
EETagent <vojta.jungmann@gmail.com>
2023-04-15 14:46:44 +0200

    feat: make password eye icon working, preload icons on mount

--- apps/web/src/lib/components/inputs/PasswordInput.svelte
+++ apps/web/src/lib/components/inputs/PasswordInput.svelte
@@ -1,14 +1,28 @@
 <script lang="ts">
+    import { loadIcons } from '@iconify/svelte';
+    import { onMount } from 'svelte';
     import TextInput from './TextInput.svelte';
 
     export let required: boolean = false;
     export let focus: boolean = false;
     export let error: boolean = false;
+
+    const hiddenPasswordEye = 'mdi:eye-outline';
+    const visiblePasswordEye = 'mdi:eye-off-outline';
+
+    onMount(() => {
+        loadIcons([hiddenPasswordEye, visiblePasswordEye]);
+    });
+
+    let isPasswordVisible = false;
 </script>
 
 <TextInput
-    icon="ic:outline-remove-red-eye"
-    type="password"
+    on:icon-click={() => {
+        isPasswordVisible = !isPasswordVisible;
+    }}
+    icon={isPasswordVisible ? visiblePasswordEye : hiddenPasswordEye}
+    type={isPasswordVisible ? 'text' : 'password'}
     placeholder="Heslo"
     name="password"
     {required}
