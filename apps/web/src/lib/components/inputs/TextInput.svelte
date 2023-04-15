fc4cb3f579bddc3fea54114dd267c24721fcc563
EETagent <vojta.jungmann@gmail.com>
2023-04-15 14:46:25 +0200

    feat: improve TextInput, make icon clickable, reactive text field type

--- apps/web/src/lib/components/inputs/TextInput.svelte
+++ apps/web/src/lib/components/inputs/TextInput.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
     import Icon from '@iconify/svelte';
+    import { createEventDispatcher } from 'svelte';
 
-    const typeAction = (node: HTMLInputElement) => {
+    const typeAction = (node: HTMLInputElement, type: string) => {
         node.type = type;
+        return {
+            update(newType: string) {
+                node.type = newType;
+            }
+        };
     };
 
     const focusAction = (node: HTMLInputElement) => {
@@ -17,13 +23,21 @@
     export let required: boolean = false;
     export let focus: boolean = false;
     export let error: boolean = false;
+
+    const dispatch = createEventDispatcher();
 </script>
 
 <div class="relative">
-    <input class:error {name} use:typeAction use:focusAction {placeholder} {required} />
+    <input class:error {name} use:typeAction={type} use:focusAction {placeholder} {required} />
     {#if icon}
         <span class="absolute inset-y-0 right-4 inline-flex items-center">
-            <Icon {icon} />
+            <button
+                on:click|preventDefault={() => {
+                    dispatch('icon-click');
+                }}
+            >
+                <Icon {icon} />
+            </button>
         </span>
     {/if}
 </div>
@@ -38,6 +52,9 @@
     input:hover,
     input:focus {
         @apply border-2 border-[#3580b7];
+    }
+    button {
+        @apply p-1;
     }
     .error {
         @apply border-2 border-red-700;
