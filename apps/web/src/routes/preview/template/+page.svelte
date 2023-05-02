ef92b3198266e2173f3afcd3a8bd5ccfa6f18b67
Vojtěch Jungmann <vojta.jungmann@gmail.com>
2023-05-02 13:22:54 +0200

    feat: remove runtime highlighting (now done at parse step)

--- apps/web/src/routes/preview/template/+page.svelte
+++ apps/web/src/routes/preview/template/+page.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
     import { enhance } from '$app/forms';
 
-    import hljs from 'highlight.js';
     import 'highlight.js/styles/github.css';
 
     import type { ActionData } from './$types';
@@ -10,12 +8,6 @@
     import TestPreview from '$lib/components/testpreview/TestPreview.svelte';
 
     export let form: ActionData;
-
-    onMount(() => {
-        document.querySelectorAll('.description code').forEach((el) => {
-            hljs.highlightElement(el as HTMLElement);
-        });
-    });
 </script>
 
 <form method="POST" use:enhance>
