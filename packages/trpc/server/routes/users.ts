9d15d35bee3f059562ba1d0c9bc01654f467f785
Vojtěch Jungmann <vojta.jungmann@gmail.com>
2024-02-25 13:30:11 +0100

    fix: add ssr only warning to xlsx trpc procedure

--- packages/trpc/server/routes/users.ts
+++ packages/trpc/server/routes/users.ts
@@ -62,6 +62,10 @@ export const users = t.router({
             trpcInfo(ctx, `Created user ${input.username}`);
         }),
     csv: t.procedure.use(adminAuth).query(async () => exportCsv()),
+    /**
+     * @description Export users to xlsx, Only SSR
+     * @returns ArrayBuffer
+     */
     xlsx: t.procedure.use(adminAuth).query(async () => exportXlsx()),
     resetPassword: t.procedure
         .use(adminAuth)
